af0b7a8f0530432ea439ce8880a6e3072aa1fb96
Patrick Sullivan <patricksullivan08@gmail.com>
2020-03-21 02:57:02 -0400

    connect button

--- src/Main.elm
+++ src/Main.elm
@@ -1,31 +1,74 @@
 module Main exposing (Msg(..), main, update, view)
 
 import Browser
-import Html exposing (Html, button, div, text)
-import Html.Events exposing (onClick)
+import Html exposing (Html, a, button, div, input, text)
+import Html.Attributes exposing (class, placeholder, value)
+import Html.Events exposing (onClick, onInput)
 
 
+main : Program () State Msg
 main =
-    Browser.sandbox { init = 0, update = update, view = view }
+    Browser.sandbox { init = init, update = update, view = view }
+
+
+type alias State =
+    { uiState : UIState
+    , gameState : Maybe GameState
+    }
+
+
+type alias GameState =
+    { mapWidth : Int
+    , mapHeight : Int
+    }
+
+
+type alias UIState =
+    { serverUrl : String
+    }
+
+
+init : State
+init =
+    { uiState = initUIState
+    , gameState = Nothing
+    }
+
+
+initUIState : UIState
+initUIState =
+    { serverUrl = ""
+    }
 
 
 type Msg
-    = Increment
-    | Decrement
+    = Connect
+    | ChangeServerUrl String
 
 
-update msg model =
+update : Msg -> State -> State
+update msg state =
     case msg of
-        Increment ->
-            model + 1
+        Connect ->
+            { state
+                | gameState = Just { mapWidth = 50, mapHeight = 50 }
+            }
 
-        Decrement ->
-            model - 1
+        ChangeServerUrl url ->
+            { state
+                | uiState = { serverUrl = url }
+            }
 
 
-view model =
-    div []
-        [ button [ onClick Decrement ] [ text "-" ]
-        , div [] [ text (String.fromInt model) ]
-        , button [ onClick Increment ] [ text "+" ]
+view : State -> Html Msg
+view state =
+    div [ class "page" ]
+        [ div [ class "top-toolbar" ]
+            [ input [ class "server-input", placeholder "Server URL", value state.uiState.serverUrl, onInput ChangeServerUrl ] []
+            , a [ class "connect-button" ] [ text "Connect" ]
+            ]
+        , div [ class "pane" ]
+            [ text "content" ]
+        , div [ class "bottom-toolbar" ]
+            [ text "toolbar" ]
         ]
